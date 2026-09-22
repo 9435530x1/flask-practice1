8b99de4625e6af0962a83fec0525303861186347
조준협 <kim6402735@naver.com>
2026-09-23 08:26:45 +0900

    feat: 인사 페이지 완성

--- HW1/app.py
+++ HW1/app.py
@@ -10,3 +10,7 @@ def index():
 def profile():
     my_hobbies = ["음악감상", "독서", "게임"]
     return render_template("profile.html", hobbies=my_hobbies)
+
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
